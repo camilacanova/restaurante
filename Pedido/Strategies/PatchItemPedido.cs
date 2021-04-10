13a889f6be18afe2c83cd0cfddd7c1c0415a2777
draxlucas <lucas.silva179@fatec.sp.gov.br>
2021-04-10 18:31:21 -0300

    Ajustes gerais

---
+++ Pedido/Strategies/PatchItemPedido.cs
@@ -0,0 +1,13 @@
+using PedidoAPI.Model;
+using PedidoAPI.Util;
+
+namespace PedidoAPI.Strategies
+{
+    public class PatchItemPedido : IStrategy<ItemPedido>
+    {
+        public Result<ItemPedido> execute(ItemPedido entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
